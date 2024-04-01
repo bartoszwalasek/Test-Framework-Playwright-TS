b8c1f0a3a53d7927480eb9b0e8ad7d411e83d590
bartosz-walasek <b.wal1990@gmail.com>
2024-04-01 12:51:27 +0200

    fix: add replace to faker

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,7 +1,7 @@
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker/locale/pl';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
@@ -13,8 +13,8 @@ test.describe('Verify register', () => {
     const loginPage = new LoginPage(page);
     const welcomePage = new WelcomePage(page);
 
-    const userFirstName = faker.person.firstName();
-    const userLastName = faker.person.lastName();
+    const userFirstName = faker.person.firstName().replace(/[^A-Za-z]g/, '');
+    const userLastName = faker.person.lastName().replace(/[^A-Za-z]g/, '');
     const userEmail = faker.internet.email({
       firstName: userFirstName,
       lastName: userLastName,
